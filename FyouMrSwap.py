d24fab9eed3f2410096704e27436ecc9157c95ca
daddyotwo <30300220+daddyotwo@users.noreply.github.com>
2017-09-22 10:54:04 +0300

    Update FyouMrSwap.py

--- FyouMrSwap.py
+++ FyouMrSwap.py
@@ -4,13 +4,13 @@ M.pop(0)
 C = M.copy()
 for ind,val in enumerate(M):
     if ind == len(M)-2:
-        if min(C[ind], C[ind+1], key=lambda x: x[0]) != C[ind]:
+        if min(C[ind], C[ind+1]) != C[ind]:
             C[ind], C[ind+1] = C[ind+1], C[ind]
             with open('output.txt', 'a') as out:
                 out.write('Swap elements at indices '+str(ind+1)+' and '+str(ind+2)+'.\n')
     elif ind == len(M)-1: 
         break    
-    temp = min(C[ind+1:], key=lambda x: x[0])
+    temp = min(C[ind+1:])
     if C[ind][0] > temp[0]:
         tempind = C.index(temp)
         C[ind],C[tempind] = C[tempind], C[ind]
